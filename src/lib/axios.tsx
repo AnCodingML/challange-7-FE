89e068c2aa0fab81bb0e4f036d60053932ecd428
Andi <{an.ekanugraha@gmail.com}>
2024-07-08 09:48:34 +0700

    rev config vite

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1/'
+  // baseURL: 'http://localhost:5000/api/v1/'
+  baseURL: 'http://8.215.36.119:5000/api/v1/'
 })
 
 export default instance
