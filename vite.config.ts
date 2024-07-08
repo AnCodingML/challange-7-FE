835f4bad3c050846c6e0b0196a2951d231ce092f
Andi <{an.ekanugraha@gmail.com}>
2024-07-08 09:51:33 +0700

    rev config vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // Ganti dengan port yang kamu inginkan
+    host: '0.0.0.0', // Mengikat pada semua antarmuka jaringan
   },
 })
