7708d42541f609394d862c95eabdb01d04bde903
Andi <{an.ekanugraha@gmail.com}>
2024-07-18 14:14:39 +0700

    rev axios url migrate

--- src/lib/axios_img-car.tsx
+++ src/lib/axios_img-car.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   // baseURL: 'http://localhost:5000/public'
-  baseURL: 'http://8.215.36.119:5000/public'
+  baseURL: 'http://54.179.193.110:5000/public'
 })
 
 export default instance
